66b75da1571c354543c76936828fc26b7cbd7326
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-08-29 00:06:35 +0200

    Migrate tests to pytest

--- backend/heating/tests/test_models.py
+++ backend/heating/tests/test_models.py
@@ -1,118 +1,121 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=no-self-use, redefined-outer-name, too-few-public-methods
+# pylint: disable=unused-argument
 
 import datetime
 
-from django.test import TestCase
 from django.utils import timezone
 
 from django_dynamic_fixture import G, N, F
+import pytest
 
 from ..models import Zone, Slot, Derogation
 
 
-class ZoneModelTest(TestCase):
+WEEKDAYS = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+
+@pytest.fixture
+def now():
+    return timezone.localtime(timezone.now())
+
+
+@pytest.fixture
+def today(now):
+    today_weekday = now.weekday()
+    return WEEKDAYS[today_weekday]
+
+
+@pytest.fixture
+def derogation_fixture(db):
+    class Fixture:
+        def __init__(self):
+            self.past_derog = G(
+                Derogation, mode='E',
+                start_dt=timezone.now() - datetime.timedelta(days=1),
+                end_dt=timezone.now() - datetime.timedelta(minutes=2)
+            )
+            self.active_derog = G(
+                Derogation, mode='E',
+                start_dt=timezone.now(),
+                end_dt=timezone.now() + datetime.timedelta(minutes=2)
+            )
+            self.future_derog = G(
+                Derogation, mode='E',
+                start_dt=timezone.now() + datetime.timedelta(minutes=2),
+                end_dt=timezone.now() + datetime.timedelta(days=1)
+            )
+    return Fixture()
+
+
+class TestZoneModel:
 
     def test_string_representation(self):
         zone = N(Zone)
-        self.assertEqual(str(zone), 'Z{}'.format(zone.num))
+        assert str(zone) == 'Z{}'.format(zone.num)
 
-    def test_get_modes_queryset_method(self):
-        wdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-        now = timezone.localtime(timezone.now())
-        zone1 = G(Zone, num=1)
-        kwargs = {
-            'zone': F(num=2), wdays[now.weekday()]: True, 'mode': 'E',
-            'start_time': (now - datetime.timedelta(minutes=2)).time(),
-            'end_time': (now + datetime.timedelta(minutes=2)).time(),
-        }
-        slot_z2 = G(Slot, **kwargs)
-        kwargs = {
-            'zone': F(num=3), wdays[now.weekday()]: True, 'mode': 'H',
-            'start_time': (now - datetime.timedelta(minutes=2)).time(),
-            'end_time': (now + datetime.timedelta(minutes=2)).time(),
-        }
-        slot_z3 = G(Slot, **kwargs)
-        derog_z3 = G(
-            Derogation, mode='A', zones=[F(num=3)],
-            start_dt=timezone.now() - datetime.timedelta(minutes=2),
-            end_dt=timezone.now() + datetime.timedelta(minutes=2),
-        )
-        self.assertEqual(
-            Zone.objects.get_modes(), {'1': 'C', '2': 'E', '3': 'A'})
+    @pytest.mark.django_db
+    def test_get_modes_queryset_method(self, now, today):
+        # Create an empty zone 1
+        G(Zone, num=1)
+        # Create an active slot in zone 2
+        G(Slot, zone=F(num=2), **{today: True}, mode='E',
+          start_time=(now - datetime.timedelta(minutes=2)).time(),
+          end_time=(now + datetime.timedelta(minutes=2)).time())
+        # Create an active slot in zone 3
+        G(Slot, zone=F(num=3), **{today: True}, mode='H',
+          start_time=(now - datetime.timedelta(minutes=2)).time(),
+          end_time=(now + datetime.timedelta(minutes=2)).time())
+        # Create an active derogation in zone 3
+        G(Derogation, mode='A', zones=[F(num=3)],
+          start_dt=timezone.now() - datetime.timedelta(minutes=2),
+          end_dt=timezone.now() + datetime.timedelta(minutes=2))
+        assert Zone.objects.get_modes() == {'1': 'C', '2': 'E', '3': 'A'}
 
 
-class SlotModelTest(TestCase):
+class TestSlotModel:
 
+    @pytest.mark.django_db
     def test_string_representation(self):
         slot = N(
             Slot, mon=True, wed=True, fri=True, sun=True, mode='E',
             start_time=datetime.time(4, 2), end_time=datetime.time(15, 54)
         )
-        self.assertEqual(
-            str(slot),
-            "Z{} 04:02:00-15:54:00 [L*M*V*D] Eco".format(slot.zone.num)
-        )
+        expected = "Z{} 04:02:00-15:54:00 [L*M*V*D] Eco".format(slot.zone.num)
+        assert str(slot) == expected
 
-    def test_active_queryset_method(self):
+    @pytest.mark.django_db
+    def test_active_queryset_method(self, now, today):
+        all_but_today = {day: True for day in
+                         [wd for wd in WEEKDAYS if wd != today]}
         zone = G(Zone, num=1)
-        wdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-        now = timezone.localtime(timezone.now())
         if now.time().hour < 1:
             raise Exception("This test cannot be run between 00:00 and 01:00")
-        kwargs = {
-            'zone': zone, wdays[now.weekday()]: True, 'mode': 'E',
-            'start_time': (now - datetime.timedelta(hours=1)).time(),
-            'end_time': (now - datetime.timedelta(minutes=2)).time()
-        }
-        past_slot = G(Slot, **kwargs)
-        kwargs = {
-            'zone': zone, wdays[now.weekday()]: True, 'mode': 'E',
-            'start_time': now.time(),
-            'end_time': (now + datetime.timedelta(minutes=2)).time()
-        }
-        active_slot = G(Slot, **kwargs)
-        kwargs = {
-            'zone': zone, wdays[now.weekday()]: True, 'mode': 'E',
-            'start_time': (now + datetime.timedelta(minutes=2)).time(),
-            'end_time': (now - datetime.timedelta(hours=1)).time()
-        }
-        future_slot = G(Slot, **kwargs)
-        kwargs = {
-            'zone': zone, wdays[(now.weekday() - 1) % 7]: True, 'mode': 'E',
-            'start_time': (now - datetime.timedelta(hours=1)).time(),
-            'end_time': (now - datetime.timedelta(minutes=2)).time()
-        }
-        yesterday_slot = G(Slot, **kwargs)
-        kwargs = {
-            'zone': zone, wdays[(now.weekday() + 1) % 7]: True, 'mode': 'E',
-            'start_time': (now - datetime.timedelta(hours=1)).time(),
-            'end_time': (now - datetime.timedelta(minutes=2)).time()
-        }
-        tomorrow_slot = G(Slot, **kwargs)
+        # Create a past slot
+        G(Slot, zone=zone, **{today: True}, mode='E',
+          start_time=(now - datetime.timedelta(hours=1)).time(),
+          end_time=(now - datetime.timedelta(minutes=2)).time())
+        # Create an active slot
+        active_slot = G(
+            Slot, zone=zone, **{today: True}, mode='E',
+            start_time=now.time(),
+            end_time=(now + datetime.timedelta(minutes=2)).time())
+        # Create a future slot
+        G(Slot, zone=zone, **{today: True}, mode='E',
+          start_time=(now + datetime.timedelta(minutes=2)).time(),
+          end_time=(now - datetime.timedelta(hours=1)).time())
+        # Create a slot active `all but today` days of week at the same time
+        G(Slot, zone=zone, **all_but_today, mode='E',
+          start_time=now.time(),
+          end_time=(now + datetime.timedelta(minutes=2)).time())
         queryset = Slot.objects.active()
-        self.assertEqual(queryset.count(), 1)
-        self.assertEqual(queryset[0], active_slot)
+        assert queryset.count() == 1
+        assert queryset[0] == active_slot
 
 
-class DerogationModelTest(TestCase):
+class TestDerogationModel:
 
-    def create_entries_for_active_tests(self):
-        self.past_derog = G(
-            Derogation, mode='E',
-            start_dt=timezone.now() - datetime.timedelta(days=1),
-            end_dt=timezone.now() - datetime.timedelta(minutes=2)
-        )
-        self.active_derog = G(
-            Derogation, mode='E',
-            start_dt=timezone.now(),
-            end_dt=timezone.now() + datetime.timedelta(minutes=2)
-        )
-        self.future_derog = G(
-            Derogation, mode='E',
-            start_dt=timezone.now() + datetime.timedelta(minutes=2),
-            end_dt=timezone.now() + datetime.timedelta(days=1)
-        )
-
+    @pytest.mark.django_db
     def test_string_representation(self):
         start = timezone.make_aware(
             datetime.datetime(2015, 2, 25, 17, 24),
@@ -124,28 +127,24 @@ class DerogationModelTest(TestCase):
         )
         derog = G(Derogation, start_dt=start, end_dt=end, mode='H',
                   zones=[F(num=2), F(num=3)])
-        self.assertEqual(str(derog), "25/02-17:24->18/03-18:12 H Z2-Z3")
+        assert str(derog) == "25/02-17:24->18/03-18:12 H Z2-Z3"
 
-    def test_active_queryset_method(self):
-        self.create_entries_for_active_tests()
+    def test_active_queryset_method(self, derogation_fixture):
         queryset = Derogation.objects.active()
-        self.assertEqual(queryset.count(), 1)
-        self.assertEqual(queryset[0], self.active_derog)
+        assert queryset.count() == 1
+        assert queryset[0] == derogation_fixture.active_derog
 
-    def test_is_active_model_method(self):
-        self.create_entries_for_active_tests()
-        self.assertFalse(self.past_derog.is_active())
-        self.assertTrue(self.active_derog.is_active())
-        self.assertFalse(self.future_derog.is_active())
+    def test_is_active_model_method(self, derogation_fixture):
+        assert not derogation_fixture.past_derog.is_active()
+        assert derogation_fixture.active_derog.is_active()
+        assert not derogation_fixture.future_derog.is_active()
 
-    def test_outdated_queryset_method(self):
-        self.create_entries_for_active_tests()
+    def test_outdated_queryset_method(self, derogation_fixture):
         queryset = Derogation.objects.outdated()
-        self.assertEqual(queryset.count(), 1)
-        self.assertEqual(queryset[0], self.past_derog)
+        assert queryset.count() == 1
+        assert queryset[0] == derogation_fixture.past_derog
 
-    def test_is_outdated_model_method(self):
-        self.create_entries_for_active_tests()
-        self.assertTrue(self.past_derog.is_outdated())
-        self.assertFalse(self.active_derog.is_outdated())
-        self.assertFalse(self.future_derog.is_outdated())
+    def test_is_outdated_model_method(self, derogation_fixture):
+        assert derogation_fixture.past_derog.is_outdated()
+        assert not derogation_fixture.active_derog.is_outdated()
+        assert not derogation_fixture.future_derog.is_outdated()
