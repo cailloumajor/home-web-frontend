a0f72e522e09b39deab4e9a82a02e1a0ca25dc39
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-09-29 21:08:52 +0200

    Enable project to use django-configurations

--- backend/home_web/wsgi.py
+++ backend/home_web/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home_web.settings")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
