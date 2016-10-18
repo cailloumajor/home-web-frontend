7fd59487e628ceeff021818f53c676bb10396d93
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-10-18 22:00:52 +0200

    Add REST framework settings for production

--- backend/home_web/settings.py
+++ backend/home_web/settings.py
@@ -148,3 +148,12 @@ class Prod(Common):
     DATABASES = values.DatabaseURLValue()
 
     EMAIL = values.EmailURLValue()
+
+    REST_FRAMEWORK = {
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+        ),
+        'DEFAULT_PARSER_CLASSES': (
+            'rest_framework.parsers.JSONParser',
+        ),
+    }
