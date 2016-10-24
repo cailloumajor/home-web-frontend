d03758031f536acfd347edb963175e59f77c7500
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-10-24 15:42:57 +0200

    Add message output to backend Docker entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 set -e
 
+myname="$(basename $0)"
+
+msg(){
+    echo "$myname: $1"
+}
+
 if ./manage.py diffsettings --all | grep -q "DEBUG = False"; then
+    msg "start collecting static files"
     ./manage.py collectstatic --noinput
 fi
 
+msg "start migrating models"
 ./manage.py migrate
 
+msg "launching command"
 exec "$@"
