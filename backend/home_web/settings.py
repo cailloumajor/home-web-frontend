58d19b7f1b3b2d22ab7a07a7a201b317c6ce69ab
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-10-24 18:51:23 +0200

    Add Django password validation setting

--- backend/home_web/settings.py
+++ backend/home_web/settings.py
@@ -80,6 +80,31 @@ class Common(Configuration):
         'sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
     )
 
+    # Password validation
+    # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME':
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME':
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator',
+        },
+        {
+            'NAME':
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator',
+        },
+        {
+            'NAME':
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator',
+        },
+    ]
+
     # Internationalization
     # https://docs.djangoproject.com/en/1.10/topics/i18n/
     LANGUAGE_CODE = 'fr-FR'
