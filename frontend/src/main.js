c17f7fddb51511112cb5937eef26fcbdee80f52c
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-01-22 12:21:18 +0100

    Add Vuetify to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
