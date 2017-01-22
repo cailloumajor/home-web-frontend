acc01660328382b33636de7aad4dab45c30c2688
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-01-22 12:21:18 +0100

    Add Vuetify to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,17 +12,13 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  mounted () {
+    this.$vuetify.init()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+@import '~vuetify/src/stylus/main'
 </style>
