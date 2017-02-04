f7cb2b51fcbbc914821540f7699e67ed982171e4
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-02-04 16:41:28 +0100

    Upgrade to latest webpack vue template

---
+++ frontend/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Heating = resolve => require(['./pages/Heating'], resolve)
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/heating',
+      component: Heating,
+      meta: {
+        title: 'Chauffage'
+      }
+    }
+  ]
+})
+
+router.afterEach(route => {
+  document.title = route.meta.title + ' - Home Web'
+})
+
+export default router
