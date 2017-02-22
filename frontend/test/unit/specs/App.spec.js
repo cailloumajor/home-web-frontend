d02428b95b518d5354095b44daece7e9d7467371
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-02-22 19:03:29 +0100

    Add App.vue unit test

---
+++ frontend/test/unit/specs/App.spec.js
@@ -0,0 +1,10 @@
+import App from 'src/App.vue'
+
+describe('App.vue entrypoint', function () {
+  it('should set the correct default data', function () {
+    expect(App.data).to.be.a('function')
+    const defaultData = App.data()
+    expect(defaultData.links).to.not.be.empty
+      .and.to.have.keys('text', 'href')
+  })
+})
