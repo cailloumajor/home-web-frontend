d12cc46fb91a9ff7515787f2831e8b81c8ddaae1
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-05-17 22:05:41 +0200

    Use data-table component for pilotwire log

--- frontend/test/unit/specs/PilotwireLog.spec.js
+++ frontend/test/unit/specs/PilotwireLog.spec.js
@@ -2,24 +2,11 @@ import Vue from 'vue'
 import PilotwireLog from '@/components/PilotwireLog'
 
 describe('PilotwireLog component', function () {
-  const log = [0, 1, 2].map(el => {
-    const dt = new Date()
-    dt.setHours(el)
-    return {
-      timestamp: dt.toISOString(),
-      level: ['INFO', 'WARNING', 'ERROR'][el],
-      message: 'Message ' + el
-    }
-  })
-
   it('should set correct default data', function () {
     expect(PilotwireLog.data).to.be.a('function')
     const defaultData = PilotwireLog.data()
     expect(defaultData.headers).to.have.lengthOf(3)
-      .and.to.all.have.keys('text', 'width')
     expect(defaultData.logColors).to.have.keys('INFO', 'WARNING', 'ERROR')
-    expect(defaultData.messageColWidth).to.be.null()
-    expect(defaultData.tbodyHeight).to.be.null()
   })
 
   describe('`isActive` property watcher', function () {
@@ -44,63 +31,6 @@ describe('PilotwireLog component', function () {
       Vue.nextTick(() => {
         expect(vm.fetch).to.have.been.calledOnce()
         done()
-      })
-    })
-  })
-
-  describe('`fetchStatus` watcher', function () {
-    it('should be declared', function () {
-      expect(PilotwireLog.watch.fetchStatus).to.be.a('function')
-    })
-
-    it('should call `resize` method when loaded', function (done) {
-      const vm = new Vue(PilotwireLog).$mount()
-      sinon.stub(vm, 'resize')
-      expect(vm.resize).to.not.have.been.called()
-      vm.fetchStatus = 'loaded'
-      Vue.nextTick(() => {
-        setTimeout(() => {
-          expect(vm.resize).to.have.been.calledOnce()
-          done()
-        }, 50)
-      })
-    })
-
-    it('should not call `resize` method when not loaded', function (done) {
-      const vm = new Vue(PilotwireLog).$mount()
-      sinon.stub(vm, 'resize')
-      expect(vm.resize).to.not.have.been.called()
-      vm.fetchStatus = 'not loaded'
-      Vue.nextTick(() => {
-        setTimeout(() => {
-          expect(vm.resize).to.not.have.been.calledOnce()
-          done()
-        }, 50)
-      })
-    })
-  })
-
-  describe('`resize` method', function () {
-    it('should be declared', function () {
-      expect(PilotwireLog.methods.resize).to.be.a('function')
-    })
-
-    it('should serve its purpose', function (done) {
-      const app = document.createElement('div')
-      app.id = 'app'
-      document.body.appendChild(app)
-      const Ctor = Vue.extend(PilotwireLog)
-      const vm = new Ctor({ el: '#app' })
-      vm.fetchData = log
-      vm.fetchStatus = 'loaded'
-      Vue.nextTick(() => {
-        setTimeout(() => {
-          expect(vm.tbodyHeight).to.be.above(0, 'tbodyHeight')
-          const widths = vm.headers.map((el) => el.width)
-          expect(widths).to.all.be.above(0, 'headers.width')
-          expect(vm.messageColWidth).to.be.above(0, 'messageColWidth')
-          done()
-        }, 50)
       })
     })
   })
