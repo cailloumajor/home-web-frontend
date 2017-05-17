c9c49291d2d0d58def78e9078d7fd95def356763
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-05-17 22:05:41 +0200

    Use data-table component for pilotwire log

--- frontend/src/components/PilotwireLog.vue
+++ frontend/src/components/PilotwireLog.vue
@@ -3,33 +3,23 @@
     :status="fetchStatus"
     error-text="Erreur de récupération du journal."
   >
-    <table>
-      <thead><tr>
-        <th
-          v-for="header in headers"
-          :style="{ width: header.width + 'px' }"
-          class="centered-cell"
-        >
-          {{ header.text }}
-        </th>
-      </tr></thead>
-      <tbody :style="{ height: tbodyHeight + 'px' }">
-        <template v-for="entry in fetchData">
-          <tr :class="logColors[entry.level] + '--text'">
-            <td v-text="new Date(entry.timestamp).toLocaleString()"></td>
-            <td class="centered-cell">{{ entry.level }}</td>
-            <td :style="{ width: messageColWidth + 'px' }">
-              {{ entry.message }}
-            </td>
-          </tr>
-        </template>
-      </tbody>
-    </table>
+    <v-data-table
+      :headers="headers"
+      :items="fetchData"
+      hide-actions
+    >
+      <template slot="items" scope="props">
+        <td
+          v-for="field in ['timestamp', 'level', 'message']"
+          v-text="props.item[field]"
+          :class="`${logColors[props.item.level]}--text`"
+        ></td>
+      </template>
+    </v-data-table>
   </loading-layout>
 </template>
 
 <script>
-import _ from 'lodash'
 import Fetching from '@/mixins/Fetching'
 import LoadingLayout from '@/components/LoadingLayout'
 
@@ -46,81 +36,26 @@ export default {
   data () {
     return {
       headers: [
-        { text: 'Date/Heure', width: null },
-        { text: 'Niveau', width: null },
-        { text: 'Message', width: null }
+        { text: 'Date/Heure', value: 'timestamp', left: true },
+        { text: 'Niveau', value: 'level', left: true },
+        { text: 'Message', value: 'message', left: true }
       ],
       logColors: {
         INFO: 'green',
         WARNING: 'orange',
         ERROR: 'red'
-      },
-      messageColWidth: null,
-      tbodyHeight: null
-    }
-  },
-
-  methods: {
-    resize () {
-      const thead = this.$el.querySelector('thead')
-      const tbody = this.$el.querySelector('tbody')
-      const tbodyColumns = this.$el.querySelectorAll('tbody tr:first-child td')
-      const columnWidths = [].map.call(tbodyColumns, el => el.offsetWidth)
-      const lastColWidth = this.$el.offsetWidth - _.sum(_.initial(columnWidths))
-      let scrollBarWidth
-
-      // Resize tbody height
-      this.tbodyHeight = (
-        this.$el.parentElement.offsetHeight - thead.offsetHeight
-      )
-
-      // Resize columns width
-      this.$nextTick(() => {
-        scrollBarWidth = thead.scrollWidth - tbody.scrollWidth
-        columnWidths.splice(-1, 1, lastColWidth)
-        columnWidths.map((width, index) => {
-          this.headers[index].width = width
-        })
-        this.$nextTick(() => {
-          this.messageColWidth = _.last(columnWidths) - scrollBarWidth
-        })
-      })
+      }
     }
   },
 
   props: ['isActive'],
 
   watch: {
-    isActive () {
-      if (this.isActive) {
+    isActive (newVal) {
+      if (newVal) {
         this.fetch('/api/heating/pilotwirelog/')
-      }
-    },
-
-    fetchStatus () {
-      if (this.fetchStatus === 'loaded') {
-        this.$nextTick(() => {
-          setTimeout(this.resize, 0)
-        })
       }
     }
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-table
-  thead
-  tbody
-    display: block
-
-  tbody
-    overflow-y: auto
-
-    td
-      padding-top: 4px
-      padding-bottom: 4px
-
-.centered-cell
-  text-align: center
-</style>
