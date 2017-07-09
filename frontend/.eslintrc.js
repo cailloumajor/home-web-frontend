2c33445abe465c5c5b37dabf8cb0e2de082e364c
Arnaud Rocher <arnaud.roche3@gmail.com>
2017-07-09 19:47:51 +0200

    Use eslint-plugin-vue instead of eslint-plugin-html

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,18 +1,26 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parserOptions: { parser: 'babel-eslint' },
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  extends: [
+    'standard',
+    'plugin:vue/recommended'
   ],
   // add your custom rules here
   rules: {
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'vue/html-end-tags': 1,
+    'vue/html-no-self-closing': 1,
+    'vue/html-quotes': 1,
+    'vue/no-duplicate-attributes': 1,
+    'vue/no-template-key': 2,
+    'vue/order-in-components': 1,
+    'vue/require-v-for-key': 0,
+    'vue/v-bind-style': 1,
+    'vue/v-on-style': 1
   },
   globals: {}
 }
